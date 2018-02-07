0dac978c9eacee600715c661260f1dd87789aeee
genu-vs <genu.vs@gmail.com>
2018-02-07 17:04:52 +0530

    Update index.php

--- index.php
+++ index.php
@@ -10,9 +10,9 @@
 
 </head>
 <?php
-$ser="localhost";
-$user="root";
-$pass="";
+$ser="den1.mysql6.gear.host";
+$user="projectone";
+$pass="Kh8gT-vPWis-";
 $db="website";
 $con=mysqli_connect($ser,$user,$pass,$db)
 ?>
