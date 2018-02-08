8f7b36815ea7ea4181ee2d6e2a721fe9ea4e90bd
genu-vs <genu.vs@gmail.com>
2018-02-08 19:45:36 +0530

    Update goto.php

--- goto.php
+++ goto.php
@@ -4,11 +4,11 @@ $user="projectone";
 $pass="Kh8gT-vPWis-";
 $db="projectone";
 $con=mysqli_connect($ser,$user,$pass,$db);
-/*$sql='UPDATE one SET views=views+1 WHERE ID=1';
+$sql='UPDATE one SET views=views+1 WHERE ID=1';
 $v=mysqli_query($con,$sql);
 if(! $v ) {
                die('Could not update data: ' . mysql_error());
-            }*/
+            }
 header("Location: question.html");
 
 
